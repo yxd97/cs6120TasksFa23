dfbae9ba6afa5b6f297ff3ad00a2f02143d35e81
Yixiao Du <yd383@cornell.edu>
2023-10-03 19:47:00 -0400

    pass seems to work, not sure why 1+1 is 1

--- lesson7/BoolAlgInInt/BoolAlgInInt.cpp
+++ lesson7/BoolAlgInInt/BoolAlgInInt.cpp
@@ -12,29 +12,65 @@ struct BoolAlgInInt : public PassInfoMixin<BoolAlgInInt> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
         for (auto &F : M) {
             std::string fname = F.getName().str();
-            if (fname.starts_with("bool_func")) {
+            LLVMContext& context = F.getContext();
+            errs() << fname << '\n';
+            if (fname.rfind("bool_func", 0) == 0) {
                 for (auto &B : F) {
+                    Type* i1_t = IntegerType::getInt1Ty(context);
+                    Constant* one = ConstantInt::get(i1_t, 1);
                     for (auto &I : B) {
                         auto* op = dyn_cast<BinaryOperator>(&I);
                         if (op) {
+                            auto opcode = op->getOpcode();
+                            Value *lhs, *rhs, *newop;
+                            Constant *cst;
                             IRBuilder<> builder(op);
-                            Value* lhs = op->getOperand(0);
-                            Value* rhs = op->getOperand(1);
-                            Value* mul = builder.CreateMul(lhs, rhs);
-                            for (auto &U: op->uses()){
-                                User* user = U.getUser();
-                                user->setOperand(U.getOperandNo(), mul);
+                            User* user;
+                            switch (opcode) {
+                                case Instruction::And:
+                                // rewrite and to mul
+                                    lhs = op->getOperand(0);
+                                    rhs = op->getOperand(1);
+                                    newop = builder.CreateMul(lhs, rhs);
+                                    for (auto &U: op->uses()){
+                                        user = U.getUser();
+                                        user->setOperand(U.getOperandNo(), newop);
+                                    }
+                                break;
+                                case Instruction::Or:
+                                // rewrite or to add
+                                    lhs = op->getOperand(0);
+                                    rhs = op->getOperand(1);
+                                    newop = builder.CreateAdd(lhs, rhs);
+                                    for (auto &U: op->uses()){
+                                        user = U.getUser();
+                                        user->setOperand(U.getOperandNo(), newop);
+                                    }
+                                break;
+                                case Instruction::Xor:
+                                // rewrite not to 1-
+                                    lhs = op->getOperand(0);
+                                    rhs = op->getOperand(1);
+                                    // errs() << I << " // ";
+                                    cst = dyn_cast<Constant>(rhs);
+                                    if (cst && cst->isOneValue()) {
+                                        // errs() << "using xor true as not\n";
+                                        newop = builder.CreateSub(one, lhs);
+                                        for (auto &U: op->uses()){
+                                            user = U.getUser();
+                                            user->setOperand(U.getOperandNo(), newop);
+                                        }
+                                    }
+                                break;
+                                default:
+                                break;
                             }
-                            errs() << I << "// BinOp\n";
-                        } else {
-                            errs() << I << '\n';
                         }
-
                     }
                 }
             }
         }
-        return PreservedAnalyses::all();
+        return PreservedAnalyses::none();
     };
 };
 
@@ -44,7 +80,7 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
     return {
         .APIVersion = LLVM_PLUGIN_API_VERSION,
-        .PluginName = "Skeleton pass",
+        .PluginName = "BoolAlgInInt pass",
         .PluginVersion = "v0.1",
         .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
             PB.registerPipelineStartEPCallback(
